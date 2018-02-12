a8b60d5597a94038285f305c2787e1b0dc66c1f7
SaiTheCodeGuy <sgirap@bsd220.org>
2018-02-12 15:51:18 -0600

    Initial Commit

---
+++ SUITUP/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SUITUP
+//
+//  Created by Sai Girap on 2/12/18.
+//  Copyright © 2018 Sai Girap. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController
+{
+    
+    
+    @IBOutlet weak var lolyouredumb: UILabel!
+    
+    
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        print("works")
+        lolyouredumb.text? = "Kiss me Devanshi! - Abhijith"
+    }
+
+    
+
+
+}
+
