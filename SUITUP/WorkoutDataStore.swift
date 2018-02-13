867a87bd45498f640f17b09acea11b94e32eb826
Ronak Chaudhuri <rchaudhuri@ronaks-macbook-air.local>
2018-02-13 14:55:46 -0600

    Storyboard start

--- SUITUP/WorkoutDataStore.swift
+++ SUITUP/WorkoutDataStore.swift
@@ -30,11 +30,12 @@
 
 import HealthKit
 
-class WorkoutDataStore {
-  
-  class func save(prancerciseWorkout: PrancerciseWorkout,
-                  completion: @escaping ((Bool, Error?) -> Swift.Void)) {
-    
-  }
-  
-}
+//class WorkoutDataStore {
+//  
+//  class func save(prancerciseWorkout: PrancerciseWorkout,
+//                  completion: @escaping ((Bool, Error?) -> Swift.Void)) {
+//    
+//  }
+//  
+//}
+
